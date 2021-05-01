6422a8bfb3c9355a072edf86367ee3097c095c8f
Ata-92 <atakal92@gmail.com>
2021-05-01 23:45:41 +0300

    Create a wordList

--- Html-Css/app.js
+++ Html-Css/app.js
@@ -1,4 +1,11 @@
-const randomWord = require("random-words");
-let theWord = randomWord({exactly: 1, maxLength: 11, join: ""});
-
-while (theWord.length !== 11) theWord = randomWord({exactly: 1, maxLength: 11, join: ""});
+let theWordList = [
+  'appropriate', 'opportunity',
+  'arrangement', 'temperature',
+  'explanation', 'mathematics',
+  'grandfather', 'immediately',
+  'information', 'grandmother',
+  'combination', 'environment',
+  'comfortable', 'electricity',
+  'development', 'independent',
+  'composition'
+];
